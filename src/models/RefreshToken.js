af9969331f2e8c86b9385701ac5a43dda312c61e
Aakash Manihar <111000108+Aakash-ak07@users.noreply.github.com>
2023-03-21 15:52:32 +0530

    Delete src directory

--- src/models/RefreshToken.js
+++
@@ -1,9 +0,0 @@
-const mongooose = require('mongoose');
-
-const tokenSchema = new mongooose.Schema({
-    // Your code goes here
-})
-
-const RefreshToken = mongooose.model('tokens', tokenSchema);
-
-module.exports = RefreshToken;
